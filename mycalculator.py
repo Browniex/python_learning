99159ceafde2febf311e7d8516c6063126402f1e
Brownie <59609411+Browniex@users.noreply.github.com>
2023-05-07 21:13:58 +0530

    Create mycalculator.py

---
+++ mycalculator.py
@@ -0,0 +1,24 @@
+# Innocent Shivani teacher ji
+a = float(input("Enter Any number:"))
+b = input("Enter Oprater:")
+c = float(input("Enter Second Number:"))
+
+print("You Entered this", a, b, c)
+
+
+if b == "+":
+  print(a + c )
+elif b == "-":
+  print(a - c )
+elif b == "/":
+  print(a / c )
+elif b == "*":
+  print(a * c )
+else:
+  print("you noob")
+  
+  
+  
+  
+  
+  
